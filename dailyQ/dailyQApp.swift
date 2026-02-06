72fd11a040573e9eba6dfbcb5ad55916bf35784f
angel_iturrieta <app.angel.dq@gmail.com>
2026-02-05 21:42:00 -0300

    Initial Commit

---
+++ dailyQ/dailyQApp.swift
@@ -0,0 +1,17 @@
+//
+//  dailyQApp.swift
+//  dailyQ
+//
+//  Created by Angel Iturrieta on 05-02-26.
+//
+
+import SwiftUI
+
+@main
+struct dailyQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
